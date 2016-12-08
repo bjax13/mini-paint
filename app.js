3853a7d0c95f6c5e70c42f3ba9ebe8d112dfcc66
Bryan Jackson <bjack13@gmail.com>
2016-12-08 11:50:36 -0700

    finished the miniproject

---
+++ app.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  var box = $('.box');
+  var reset = $('#reset');
+  var color = 'white';
+  var colors = 'red blue green yellow white';
+
+  box.on('mouseenter',function (){
+    $(this).addClass(color);
+  })
+
+  box.on('dblclick',function () {
+    $(this).removeClass(color);
+  })
+
+  reset.on('click',function () {
+    box.removeClass(colors);
+  })
+  $('#red').on('click', function () {
+    color = 'red';
+  })
+  $('#blue').on('click', function () {
+    color = 'blue';
+  })
+  $('#green').on('click', function () {
+    color = 'green';
+  })
+  $('#yellow').on('click', function () {
+    color = 'yellow';
+  })
+  $('#white').on('click', function () {
+    color = 'white';
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
